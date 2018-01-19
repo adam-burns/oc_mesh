9b376ba3fedc352890bfca67f992062fe78cbedd
root <root@fedora27-lab3-1.fritz.box>
2018-01-19 15:39:45 +0100

    adds ns to print namespace

---
+++ ns.sh
@@ -0,0 +1,10 @@
+export SERVICEGRAPH=$(oc get route servicegraph -o jsonpath='{.spec.host}{"\n"}')/dotviz
+export GRAFANA=$(oc get route grafana -o jsonpath='{.spec.host}{"\n"}'):3000/dashboard/db/istio-dashboard
+export ZIPKIN=$(oc get route zipkin -o jsonpath='{.spec.host}{"\n"}')
+
+export PRODUCTPAGE=$(oc get route productpage -o jsonpath='{.spec.host}{"\n"}')
+
+echo "SERVICEGRAPH=$SERVICEGRAPH"
+echo "GRAFANA=$GRAFANA"
+echo "ZIPKIN=$ZIPKIN"
+echo "Bookapp URL= $PRODUCTPAGE"
